f3fab8e2cffce52264b4dbbb9de3789bddf20166
anpopo <65079652+anpopo@users.noreply.github.com>
2020-07-02 17:08:39 +0900

    Update calendar method fuction

--- src/anpopo/calendar/Calendar.java
+++ src/anpopo/calendar/Calendar.java
@@ -3,69 +3,32 @@ package anpopo.calendar;
 import java.util.Scanner;
 
 public class Calendar {
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
+	}
+
+	public void printSampleCalendar() {
+		System.out.println("일    월    화    수    목    금   토");
+		System.out.println("--------------------");
+		System.out.println("1  2  3  4  5  6  7");
+		System.out.println("8  9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");
+	}
+
 	public static void main(String[] args) {
 
-//		System.out.println("일    월    화    수    목    금   토");
-//		System.out.println("--------------------");
-//		System.out.println("1  2  3  4  5  6  7");
-//		System.out.println("8  9 10 11 12 13 14");
-//		System.out.println("15 16 17 18 19 20 21");
-//		System.out.println("22 23 24 25 26 27 28");
-		int[] monthList = new int[3];
-		
-		monthList[0] = 28;
-		monthList[1] = 30;
-		monthList[2] = 31;
-		
-		
 		System.out.println("달을 입력하세요.");
 		Scanner scanner = new Scanner(System.in);
-		String str = scanner.next();
-		int a = Integer.parseInt(str);
-		if (0 < a && a < 13) {
-			switch(a) {
-				case 1:
-					System.out.printf(("%d월은 %d일까지 있습니다."), a, monthList[2]);
-					break;
-				case 2:
-					System.out.printf(("%d월은 %d일까지 있습니다."), a, monthList[0]);
-					break;
-				case 3:
-					System.out.printf(("%d월은 %d일까지 있습니다."), a, monthList[2]);
-					break;
-				case 4:
-					System.out.printf(("%d월은 %d일까지 있습니다."), a, monthList[1]);
-					break;
-				case 5:
-					System.out.printf(("%d월은 %d일까지 있습니다."), a, monthList[2]);
-					break;
-				case 6:
-					System.out.printf(("%d월은 %d일까지 있습니다."), a, monthList[1]);
-					break;
-				case 7:
-					System.out.printf(("%d월은 %d일까지 있습니다."), a, monthList[2]);
-					break;
-				case 8:
-					System.out.printf(("%d월은 %d일까지 있습니다."), a, monthList[2]);
-					break;
-				case 9:
-					System.out.printf(("%d월은 %d일까지 있습니다."), a, monthList[1]);
-					break;
-				case 10:
-					System.out.printf(("%d월은 %d일까지 있습니다."), a, monthList[2]);
-					break;
-				case 11:
-					System.out.printf(("%d월은 %d일까지 있습니다."), a, monthList[1]);
-					break;
-				case 12:
-					System.out.printf(("%d월은 %d일까지 있습니다."), a, monthList[2]);
-					break;
-					
-			}
+		Calendar cal = new Calendar();
+		int a = scanner.nextInt();
+		if (a > 0 && a < 13) {
+			System.out.printf("%d월은  %d일까지 있습니다.%n", a, cal.getMaxDaysOfMonth(a));
 		} else {
-			System.out.println("월을 잘못 입력하셨습니다.");
-			
+			System.out.println("달을 잘못 입력하셨습니데이~");
 		}
+
 	}
 }
- 
